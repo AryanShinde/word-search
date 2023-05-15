918fd166bc089cdd0bd1b481c53c7e05da96cd04
Aryan Shinde <aryan.shinde20@vit.edu>
2023-05-15 12:49:35 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,81 @@
-import React, { useEffect, useRef, useState } from "react";
-import uploadIcon from "./upload.svg";
-import "./App.css";
-import { clearGrid, drawGrid, drawMatch, parseFile } from "./util";
-import { Wordsearch } from "./wordsearch";
+import React, { useEffect, useRef, useState } from 'react';
+import uploadIcon from './upload.svg';
+import './App.css';
+import { clearGrid, drawGrid, drawMatch, parseFile } from './util';
+import { Wordsearch } from './wordsearch';
 
 function App() {
-    const canvas = useRef<HTMLCanvasElement>(null);
-    const fileInput = useRef<HTMLInputElement>(null);
-    const [puzzle, setPuzzle] = useState<Wordsearch>();
+  const canvas = useRef<HTMLCanvasElement>(null);
+  const fileInput = useRef<HTMLInputElement>(null);
+  const [puzzle, setPuzzle] = useState<Wordsearch>();
 
-    const openFileInput = () => {
-        fileInput.current?.click();
-    };
+  const openFileInput = () => {
+    fileInput.current?.click();
+  };
 
-    const onFileChange = async () => {
-        console.log("on change");
-        var file = fileInput.current?.files?.[0];
-        if (!file) return;
+  const onFileChange = async () => {
+    console.log('on change');
+    var file = fileInput.current?.files?.[0];
+    if (!file) return;
 
-        let wordsearch = await parseFile(file);
-        setPuzzle(wordsearch);
-    };
+    let wordsearch = await parseFile(file);
+    setPuzzle(wordsearch);
+  };
 
-    const solvePuzzle = () => {
-        if (!(canvas.current && puzzle)) return;
-        const matches = puzzle.solve();
-        for (let match of matches) {
-            if (match.length > 0) {
-                drawMatch(canvas.current, match);
-            }
-        }
-    };
+  const solvePuzzle = () => {
+    if (!(canvas.current && puzzle)) return;
+    const matches = puzzle.solve();
+    for (let match of matches) {
+      if (match.length > 0) {
+        drawMatch(canvas.current, match);
+      }
+    }
+  };
 
-    const clearPuzzle = () => {
-        if (!(canvas.current && puzzle)) return;
+  const clearPuzzle = () => {
+    if (!(canvas.current && puzzle)) return;
 
-        setPuzzle(undefined);
-        clearGrid(canvas.current);
+    setPuzzle(undefined);
+    clearGrid(canvas.current);
 
-        if (fileInput.current) {
-            fileInput.current.value = "";
-        }
-    };
+    if (fileInput.current) {
+      fileInput.current.value = '';
+    }
+  };
 
-    useEffect(() => {
-        if (!(canvas.current && puzzle)) return;
-        drawGrid(canvas.current, puzzle);
-    }, [puzzle]);
+  useEffect(() => {
+    if (!(canvas.current && puzzle)) return;
+    drawGrid(canvas.current, puzzle);
+  }, [puzzle]);
 
-    return (
-        <div className="App">
-            <header className="App-header">Wordsearch Solver</header>
-            <input
-                type="file"
-                accept="text/plain"
-                ref={fileInput}
-                onChange={onFileChange}
-                style={{ display: "none" }}
-            />
-            <button
-                type="button"
-                onClick={openFileInput}
-                className="App-fileInput"
-            >
-                <img
-                    src={uploadIcon}
-                    alt="Upload a file"
-                    className="App-icon"
-                />
+  return (
+    <div className="App">
+      <header className="App-header">Wordsearch Solver</header>
+      <input
+        type="file"
+        accept="text/plain"
+        ref={fileInput}
+        onChange={onFileChange}
+        style={{ display: 'none' }}
+      />
+      <button type="button" onClick={openFileInput} className="App-fileInput">
+        <img src={uploadIcon} alt="Upload a file" className="App-icon" />
+      </button>
+      <div className="App-buttons">
+        {puzzle && (
+          <>
+            <button type="button" onClick={solvePuzzle}>
+              Solve
             </button>
-            <div className="App-buttons">
-                {puzzle && (
-                    <>
-                        <button type="button" onClick={solvePuzzle}>
-                            Solve
-                        </button>
-                        <button type="button" onClick={clearPuzzle}>
-                            Clear
-                        </button>
-                    </>
-                )}
-            </div>
-            <canvas ref={canvas} />
-        </div>
-    );
+            <button type="button" onClick={clearPuzzle}>
+              Clear
+            </button>
+          </>
+        )}
+      </div>
+      <canvas style={{ color: 'white' }} ref={canvas} />
+    </div>
+  );
 }
 
 export default App;
--- src/util.ts
+++ src/util.ts
@@ -1,127 +1,128 @@
-import { GridCell, WordMatch } from "./types";
-import { Wordsearch } from "./wordsearch";
+import { GridCell, WordMatch } from './types';
+import { Wordsearch } from './wordsearch';
 
 const LETTER_PADDING = 0.5;
 const LETTER_MARGIN = 50;
 
 export const parseFile = (file: File): Promise<Wordsearch> => {
-    const reader = new FileReader();
+  const reader = new FileReader();
 
-    return new Promise((resolve, reject) => {
-        reader.addEventListener("load", () => {
-            let result = reader.result as string;
-            let lines = result.trim().split("\n");
+  return new Promise((resolve, reject) => {
+    reader.addEventListener('load', () => {
+      let result = reader.result as string;
+      let lines = result.trim().split('\n');
 
-            let grid: string[][] = [];
-            let words: string[] = [];
-            let pastDelimiter = false;
+      let grid: string[][] = [];
+      let words: string[] = [];
+      let pastDelimiter = false;
 
-            for (let i = 0; i < lines.length; i++) {
-                let line = lines[i].trim();
+      for (let i = 0; i < lines.length; i++) {
+        let line = lines[i].trim();
 
-                if (line === "///") {
-                    pastDelimiter = true;
-                    continue;
-                }
+        if (line === '///') {
+          pastDelimiter = true;
+          continue;
+        }
 
-                if (pastDelimiter) {
-                    words.push(line);
-                } else {
-                    // Splits up the string of letters into an array
-                    grid.push(line.split(""));
+        if (pastDelimiter) {
+          words.push(line);
+        } else {
+          // Splits up the string of letters into an array
+          grid.push(line.split(''));
 
-                    // If the length of the last line is not
-                    // the same as the length of this line
-                    if (grid[i - 1] && grid[i].length !== grid[i - 1].length) {
-                        throw new Error("Invalid grid");
-                    }
-                }
-            }
+          // If the length of the last line is not
+          // the same as the length of this line
+          if (grid[i - 1] && grid[i].length !== grid[i - 1].length) {
+            throw new Error('Invalid grid');
+          }
+        }
+      }
 
-            if (words.length === 0) {
-                throw new Error("No words provided");
-            }
+      if (words.length === 0) {
+        throw new Error('No words provided');
+      }
 
-            let width = grid[0].length;
-            let height = grid.length;
+      let width = grid[0].length;
+      let height = grid.length;
 
-            let puzzle = new Wordsearch(grid, words, width, height);
+      let puzzle = new Wordsearch(grid, words, width, height);
 
-            resolve(puzzle);
-            console.log(puzzle);
-        });
-
-        reader.readAsText(file);
+      resolve(puzzle);
+      console.log(puzzle);
     });
+
+    reader.readAsText(file);
+  });
 };
 
 const toCanvasCoords = (position: GridCell) => {
-    return [
-        (position[0] + LETTER_PADDING) * LETTER_MARGIN,
-        (position[1] + LETTER_PADDING) * LETTER_MARGIN,
-    ];
+  return [
+    (position[0] + LETTER_PADDING) * LETTER_MARGIN,
+    (position[1] + LETTER_PADDING) * LETTER_MARGIN,
+  ];
 };
 
 const resizeCanvas = (
-    canvas: HTMLCanvasElement,
-    width: number,
-    height: number
+  canvas: HTMLCanvasElement,
+  width: number,
+  height: number
 ) => {
-    let [canvasWidth, canvasHeight] = toCanvasCoords([width, height]);
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
+  let [canvasWidth, canvasHeight] = toCanvasCoords([width, height]);
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
 };
 
 export const clearGrid = (canvas: HTMLCanvasElement) => {
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-    canvas.width = 0;
-    canvas.height = 0;
+  const ctx = canvas.getContext('2d');
+  if (!ctx) return;
+  canvas.width = 0;
+  canvas.height = 0;
 };
 
 export const drawGrid = (canvas: HTMLCanvasElement, puzzle: Wordsearch) => {
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
+  const ctx = canvas.getContext('2d');
+  if (!ctx) return;
 
-    // Also clears the grid
-    resizeCanvas(canvas, puzzle.width, puzzle.height);
+  // Also clears the grid
+  resizeCanvas(canvas, puzzle.width, puzzle.height);
 
-    // Set up font attributes
-    ctx.font = "30px monospace";
-    ctx.textBaseline = "middle";
-    ctx.textAlign = "center";
+  // Set up font attributes
+  ctx.font = '30px monospace';
+  ctx.textBaseline = 'middle';
+  ctx.textAlign = 'center';
+  ctx.fillStyle = 'white';
 
-    for (let i = 0; i < puzzle.height; i++) {
-        let row = puzzle.grid[i];
+  for (let i = 0; i < puzzle.height; i++) {
+    let row = puzzle.grid[i];
 
-        for (let j = 0; j < puzzle.width; j++) {
-            const [x, y] = toCanvasCoords([j, i]);
-            ctx.fillText(row[j], x, y);
-        }
+    for (let j = 0; j < puzzle.width; j++) {
+      const [x, y] = toCanvasCoords([j, i]);
+      ctx.fillText(row[j], x, y);
     }
+  }
 };
 
 export const drawMatch = (canvas: HTMLCanvasElement, match: WordMatch) => {
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
+  const ctx = canvas.getContext('2d');
+  if (!ctx) return;
 
-    let startPosition = toCanvasCoords(match[0]);
-    let endPosition = toCanvasCoords(match[match.length - 1]);
+  let startPosition = toCanvasCoords(match[0]);
+  let endPosition = toCanvasCoords(match[match.length - 1]);
 
-    // To show up behind the grid
-    ctx.globalCompositeOperation = "destination-over";
+  // To show up behind the grid
+  ctx.globalCompositeOperation = 'destination-over';
 
-    // Red with 40% opacity
-    ctx.strokeStyle = "rgba(255, 0, 0, 0.4)";
+  // Red with 40% opacity
+  ctx.strokeStyle = 'rgba(255, 0, 0, 0.4)';
 
-    ctx.lineCap = "round";
-    ctx.lineWidth = 30;
+  ctx.lineCap = 'round';
+  ctx.lineWidth = 30;
 
-    ctx.beginPath();
+  ctx.beginPath();
 
-    // Position is in the format [row (y), column (x)]
-    // , but we need the x component, and then the y
-    ctx.moveTo(startPosition[1], startPosition[0]);
-    ctx.lineTo(endPosition[1], endPosition[0]);
-    ctx.stroke();
+  // Position is in the format [row (y), column (x)]
+  // , but we need the x component, and then the y
+  ctx.moveTo(startPosition[1], startPosition[0]);
+  ctx.lineTo(endPosition[1], endPosition[0]);
+  ctx.stroke();
 };
